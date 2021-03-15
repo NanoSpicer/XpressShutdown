5e10fd8f08ae9cfea2a347f5fedf3f5f9543f01c
Miquel Àngel Román Colom <miquelrc95@outlook.es>
2021-03-16 00:23:08 +0100

    running the program reports it's full endpoint

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const command = 'shutdown /s /f /t 0'; // Inmediate shutdown
+const command = 'shutdown /s /t 0'; // Inmediate shutdown
 const proc = require('child_process');
+const os = require('os');
 const express = require('express');
 const app = new express();
 const router = express.Router();
+
+
+function getIP4() {
+    const ifaces = os.networkInterfaces();
+    const addresses = ifaces[`Conexión de área local`];
+    console.log(addresses.length)
+    return addresses.find(addr => addr.family === 'IPv4')?.address;
+}
+
 
 router.get('/shutdown', (request, response) => {
     proc.exec(command, (err, stdout, stderr) => {
@@ -12,5 +22,9 @@ router.get('/shutdown', (request, response) => {
 
 app.use('/remote', router);
 app.listen(80);
-console.log('Server is running; you may lay down now');
+
+console.log(
+    `Server is running at http://${getIP4()}/remote/shutdown`,
+    `Have some rest!`
+)
 
